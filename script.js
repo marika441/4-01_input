adc5f9d7b1bbf56259f411d478f7e09b1e3fc6f6
marika441 <92457238+marika441@users.noreply.github.com>
2022-02-07 23:33:58 +0900

    [Finish]4-1入力内容の取得とイベント

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 'use strict';
 
-document.getElementById('form').onsubmit = function() {
-  console.log('クリックされました');
+document.getElementById('form').onsubmit = function(event) {
+  // console.log('クリックされました');
+  event.preventDefault();
+  const search = document.getElementById('form').word.value;
+  document.getElementById('output').textContent = `「${search}」の検索中...`;
 };
